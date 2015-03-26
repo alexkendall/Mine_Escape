f25236de98c7a008c31207007b4372298600e1c9
Alexander Harrison <alexkendall.harrison@gmail.com>
2015-03-26 12:43:31 -0400

    created game class to encapsulate games. simplified viewcontroller

--- ViewController.swift
+++ ViewController.swift
@@ -7,78 +7,13 @@
 //®
 import UIKit
 var super_view = UIView();
+var game:GameMap = GameMap();
 
 class ViewController: UIViewController {
-    
+
     func pressed_loc(sender:Mine_cell!)
     {
-        if(!GAME_STARTED)
-        {
-            if(sender.loc_id == START_LOC)
-            {
-                map[START_LOC].setTitleColor(UIColor.clearColor(), forState: UIControlState.Normal);
-                GAME_STARTED = true;
-            }
-        }
-        
-        if(GAME_STARTED)
-        {
-        if(sender.mine_exists)  // user lost game
-        {
-            GAME_OVER = true;
-            for(var i = 0; i < (NUM_ROWS * NUM_COLS); ++i)
-            {
-                map[i].timer.invalidate();
-                if(map[i].mine_exists)
-                {
-                    map[i].mark_mine();
-                    map[i].backgroundColor = UIColor.redColor();
-                }
-            }
-            super_view.bringSubviewToFront(sender);
-            sender.layer.borderWidth = 5.0;
-        }
-        else if(!sender.explored)
-        {
-            map[sender.tag].mark_explored();
-            var neighbors = map[sender.tag].unmarked_neighbors();
-            if((neighbors.count > 0) && ((NUM_LOCS - COUNT) > 1))
-            {
-                var temp_index:Int = Int(arc4random_uniform(UInt32(neighbors.count)));
-                var index = neighbors[temp_index];
-                if((temp_index % 2) == 1)
-                {
-                    map[index].speed = SPEED.MED;
-                    map[index].mark_mine();
-                }
-                else
-                {
-                    map[index].mark_mine();
-                }
-            }
-
-        }
-        if(Mine_cell.won_game())
-        {
-            for(var i = NUM_LOCS - 1; i >= 0; --i)
-            {
-                if(!map[i].mine_exists)
-                {
-                    //map[i].backgroundColor = UIColor.orangeColor();
-                    super_view.sendSubviewToBack(map[i]);
-                    map[i].layer.borderWidth = 1.0;
-                }
-                else
-                {
-                    map[i].mark_mine();
-                    map[i].backgroundColor = UIColor.redColor();
-                    map[i].layer.borderWidth = 1.0;
-                    super_view.sendSubviewToBack(map[i]);
-                }
-                map[i].timer.invalidate();
-            }
-        }
-        }
+        game.mark_location(sender.loc_id);
     }
 
     override func viewDidLoad()
@@ -88,57 +23,16 @@ class ViewController: UIViewController {
         super_view = self.view;
         super_view.backgroundColor = UIColor.blackColor(); // hide date and time
         super_view.setTranslatesAutoresizingMaskIntoConstraints(false);
-    
-        var size:Float = 1.0;
-        // create grid of elements
-        for(var row = 0; row < NUM_ROWS; ++row)
-        {
-            for(var col = 0; col < NUM_COLS; ++col)
-            {
-                var width_const:CGFloat = CGFloat(1.0 / CGFloat(NUM_COLS));
-                var height_const:CGFloat = CGFloat(1.0 / CGFloat(NUM_ROWS));
-                
-                var loc = (row * NUM_COLS) + col;
-                var subview:Mine_cell = Mine_cell(location_identifier:loc);
-                subview.setTranslatesAutoresizingMaskIntoConstraints(false);
-                
-                var width = NSLayoutConstraint(item: subview, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: super_view, attribute: NSLayoutAttribute.Width, multiplier: width_const, constant: 0.0);
-                
-                var height = NSLayoutConstraint(item: subview, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: super_view, attribute: NSLayoutAttribute.Height, multiplier: height_const, constant: 0.0);
-                
-                var increment_centerx:CGFloat  = (CGFloat(super_view.bounds.width) / CGFloat(NUM_COLS) * 0.5) + (CGFloat(super_view.bounds.width) / CGFloat(NUM_COLS) * CGFloat(col));
-                
-                var increment_x = NSLayoutConstraint(item: subview, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: super_view, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: increment_centerx);
-                
-                var increment_centery:CGFloat  = ((CGFloat(super_view.bounds.height) - TOP_BORDER) / CGFloat(NUM_ROWS) * 0.5) + ((CGFloat(super_view.bounds.height - TOP_BORDER)) / CGFloat(NUM_ROWS) * CGFloat(row));
-                
-                var increment_y = NSLayoutConstraint(item: subview, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: super_view, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: increment_centery + TOP_BORDER);
-                
-                subview.backgroundColor = UIColor.whiteColor();
-                subview.layer.borderWidth = 1.0;
-                super_view.addSubview(subview);
-                super_view.addConstraint(width);
-                super_view.addConstraint(height);
-                super_view.addConstraint(increment_x);
-                super_view.addConstraint(increment_y);
-                // tag element to uniquely identify it
-                subview.tag = (row * NUM_COLS) + col;
-                subview.addTarget(self, action: "pressed_loc:", forControlEvents: UIControlEvents.TouchDown);
-                map.append(subview);
-            }
-        }
-        map[START_LOC].backgroundColor = UIColor.grayColor();
-        map[START_LOC].setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal);
-        map[START_LOC].setTitle("START", forState: UIControlState.Normal);
-        map[START_LOC].titleLabel?.font = UIFont(name: "Arial-BoldMT" , size: 15.0);
         
-        super_view.layer.borderWidth = 2.0;
-        super_view.layer.borderColor = UIColor.blackColor().CGColor;
+        game = GameMap(num_rows: 7, num_cols: 5);
+        for(var i = 0; i < game.NUM_LOCS; ++i)
+        {
+            game.map[i].addTarget(self, action: "pressed_loc:", forControlEvents: UIControlEvents.TouchDown);
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        
     }
 }
 
--- engine.swift
+++ engine.swift
@@ -12,24 +12,19 @@ import UIKit
 
 //-------------------------------------------------------------------------
 // GLOBAL VARIABLES
-let NUM_ROWS:Int = 7;
-let NUM_COLS:Int = 5;
-let NUM_LOCS = NUM_ROWS * NUM_COLS;
+
 let TOP_BORDER:CGFloat = 20.0;
-var COUNT:Int = 0;
-var GAME_OVER:Bool = false;
-var GAME_STARTED = false;
-var START_LOC = Int(floor(Float(NUM_LOCS) / 2.0));
-var map = Array<Mine_cell>();
 
 // colors
 let LIGHT_BLUE = UIColor(red: 0.0, green: 0.8, blue: 1.0, alpha: 1.0);
 let DARK_BLUE = UIColor(red: 0.0, green: 0.0, blue: 0.3, alpha: 1.0);
+let BLUE = UIColor.blueColor();
 
 //SPEED
-enum SPEED{case SLOW, MED, FAST};
+enum SPEED{case SLOW, FAST};
 
 //-------------------------------------------------------------------------
+// CLASS THAT HOLDS DATA FOR EACH MINE CELL
 
 class Mine_cell:UIButton
 {
@@ -80,10 +75,6 @@ class Mine_cell:UIButton
         super.init();
         loc_id = location_identifier;
     }
-    class func won_game() ->(Bool)
-    {
-        return (COUNT == (NUM_ROWS * NUM_COLS))
-    }
     func update()
     {
         if(time_til_disappears > 0)
@@ -105,7 +96,7 @@ class Mine_cell:UIButton
                     setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal);
                 }
             }
-            else if(speed == SPEED.MED)
+            else if(speed == SPEED.FAST)
             {
                 switch time_til_disappears
                 {
@@ -136,9 +127,8 @@ class Mine_cell:UIButton
         mine_exists = true;
         setTitle(insignia, forState: UIControlState.Normal);
         titleLabel?.font = UIFont(name: "Arial-BoldMT" , size: 30.0);
-        ++COUNT;
         
-        if((timer_running == false) && (!GAME_OVER))
+        if(timer_running == false)
         {
             if(speed == SPEED.SLOW)
             {
@@ -146,15 +136,11 @@ class Mine_cell:UIButton
                 timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "update", userInfo: nil, repeats: true);
                 timer_running = true;
             }
-            else if(speed == SPEED.MED)
+            else if(speed == SPEED.FAST)
             {
                 time_til_disappears = 4;
                 timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "update", userInfo: nil, repeats: true);
                 timer_running = true;
-            }
-            else
-            {
-                
             }
         }
     }
@@ -164,39 +150,131 @@ class Mine_cell:UIButton
         {
             explored = true;
             backgroundColor = UIColor.grayColor();
-            ++COUNT;
         }
     }
-    func neighbors()->(Array<Int>)
+}
+
+//-------------------------------------------------------------------------
+// HOLDS DATA TO ENCAPSULATE A SINGLE GAME
+
+class GameMap
+{
+    var map = Array<Mine_cell>();
+    let NUM_ROWS:Int;
+    let NUM_COLS:Int;
+    let NUM_LOCS:Int;
+    var COUNT:Int;
+    var GAME_OVER:Bool = false;
+    var START_LOC:Int;
+    var GAME_STARTED:Bool;
+    
+    init()
+    {
+        NUM_ROWS = 4;
+        NUM_COLS = 6;
+        COUNT = 0;
+        NUM_LOCS = 24;
+        START_LOC = Int(floor(Float(NUM_LOCS) / 2.0));
+        GAME_STARTED = false;
+
+    }
+    init(num_rows:Int, num_cols:Int)
+    {
+        NUM_ROWS = num_rows;
+        NUM_COLS = num_cols;
+        COUNT = 0;
+        GAME_OVER = false;
+        NUM_LOCS = num_rows * num_cols;
+        START_LOC = Int(floor(Float(NUM_LOCS) / 2.0));
+        GAME_STARTED = false;
+
+        
+        var size:Float = 1.0;
+        // create grid of elements
+        for(var row = 0; row < self.NUM_ROWS; ++row)
+        {
+            for(var col = 0; col < self.NUM_COLS; ++col)
+            {
+                var width_const:CGFloat = CGFloat(1.0 / CGFloat(self.NUM_COLS));
+                var height_const:CGFloat = CGFloat(1.0 / CGFloat(self.NUM_ROWS));
+                
+                var loc = (row * NUM_COLS) + col;
+                var subview:Mine_cell = Mine_cell(location_identifier:loc);
+                subview.setTranslatesAutoresizingMaskIntoConstraints(false);
+                
+                var width = NSLayoutConstraint(item: subview, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: super_view, attribute: NSLayoutAttribute.Width, multiplier: width_const, constant: 0.0);
+                
+                var height = NSLayoutConstraint(item: subview, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: super_view, attribute: NSLayoutAttribute.Height, multiplier: height_const, constant: 0.0);
+                
+                var increment_centerx:CGFloat  = (CGFloat(super_view.bounds.width) / CGFloat(NUM_COLS) * 0.5) + (CGFloat(super_view.bounds.width) / CGFloat(NUM_COLS) * CGFloat(col));
+                
+                var increment_x = NSLayoutConstraint(item: subview, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: super_view, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: increment_centerx);
+                
+                var increment_centery:CGFloat  = ((CGFloat(super_view.bounds.height) - TOP_BORDER) / CGFloat(NUM_ROWS) * 0.5) + ((CGFloat(super_view.bounds.height - TOP_BORDER)) / CGFloat(NUM_ROWS) * CGFloat(row));
+                
+                var increment_y = NSLayoutConstraint(item: subview, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: super_view, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: increment_centery + TOP_BORDER);
+                
+                subview.backgroundColor = UIColor.whiteColor();
+                subview.layer.borderWidth = 1.0;
+                super_view.addSubview(subview);
+                super_view.addConstraint(width);
+                super_view.addConstraint(height);
+                super_view.addConstraint(increment_x);
+                super_view.addConstraint(increment_y);
+                // tag element to uniquely identify it
+                subview.tag = (row * NUM_COLS) + col;
+                var tag:Int = (row * NUM_COLS) + col;
+                //subview.addTarget(super_view, action: "pressed_loc:", forControlEvents: UIControlEvents.TouchDown);
+                self.map.append(subview);
+                //self.map[tag].addTarget(self, action: "pressed_loc:", forControlEvents: UIControlEvents.TouchDown);
+            }
+        }
+        self.map[self.START_LOC].backgroundColor = UIColor.grayColor();
+        self.map[self.START_LOC].setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal);
+        self.map[self.START_LOC].setTitle("START", forState: UIControlState.Normal);
+        self.map[self.START_LOC].titleLabel?.font = UIFont(name: "Arial-BoldMT" , size: 15.0);
+        
+        super_view.layer.borderWidth = 2.0;
+        super_view.layer.borderColor = UIColor.blackColor().CGColor;
+        
+    }
+    
+    func won_game()->Bool
+    {
+        return (COUNT == NUM_LOCS);
+    }
+    
+    func neighbors(var index:Int)->(Array<Int>)
     {
         var neighbor_indicies:Array<Int> = Array<Int>();
         
         // add right neigbor (if it exists)
-        if(((loc_id + 1) % NUM_COLS) != 0)
+        if(((index + 1) % NUM_COLS) != 0)
         {
-            neighbor_indicies.append(loc_id + 1);
+            neighbor_indicies.append(index + 1);
         }
         
         // left neighbor
-        if(((loc_id % NUM_COLS) != 0) && (loc_id > 0))
+        if(((index % NUM_COLS) != 0) && (index > 0))
         {
-            neighbor_indicies.append(loc_id - 1);
+            neighbor_indicies.append(index - 1);
         }
         // top neighbor
-        if((loc_id - NUM_COLS) >= 0)
+        if((index - NUM_COLS) >= 0)
         {
-            neighbor_indicies.append(loc_id - NUM_COLS)
+            neighbor_indicies.append(index - NUM_COLS)
         }
         // bottom neighbor
-        if((loc_id + NUM_COLS) < map.count)
+        if((index + NUM_COLS) < map.count)
         {
-            neighbor_indicies.append(loc_id + NUM_COLS);
+            neighbor_indicies.append(index + NUM_COLS);
         }
         return neighbor_indicies;
     }
-    func unmarked_neighbors()->(Array<Int>)
+    
+    func unmarked_neighbors(var index:Int)->(Array<Int>)
     {
-        var neighbor_indicies:Array<Int> = neighbors();
+        var neighbor_indicies:Array<Int> = neighbors(index);
         var unexplored:Array<Int> = Array<Int>();
         for(var i = 0; i < neighbor_indicies.count; ++i)
         {
@@ -207,44 +285,87 @@ class Mine_cell:UIButton
         }
         return unexplored;
     }
-    func printNeighbors()
+    
+    func printNeighbors(var index:Int)
     {
         println("--------------------------------------");
-        var neighbor_indicies:Array<Int> = neighbors();
-        println(String(format:"Neighbors of location %i", loc_id));
+        var neighbor_indicies:Array<Int> = neighbors(index);
+        println(String(format:"Neighbors of location %i", index));
         for(var i = 0; i <  neighbor_indicies.count; ++i)
         {
             println(String(neighbor_indicies[i]));
         }
         println("--------------------------------------\n");
     }
-}
-
-//-------------------------------------------------------------------------
-// WILL RESTRUCTURE CLASSES LATER
-// AND GAME_MAP TO encapsulate game
-class GameMap
-{
-    var map = Array<Mine_cell>();
-    let NUM_ROWS:Int;
-    let NUM_COLS:Int;
-    let NUM_LOCS:Int;
-    var COUNT:Int;
-    var GAME_OVER:Bool = false;
-    init()
+    
+    func mark_mine(var loc_id:Int)
     {
-        NUM_ROWS = 4;
-        NUM_COLS = 6;
-        COUNT = 0;
-        NUM_LOCS = 24;
+        map[loc_id].mark_mine();
+        ++COUNT;
     }
-    init(num_rows:Int, num_cols:Int)
+    
+    func end_game()
     {
-        NUM_ROWS = num_rows;
-        NUM_COLS = num_cols;
-        COUNT = 0;
-        GAME_OVER = false;
-        NUM_LOCS = num_rows * num_cols;
+        for(var i = 0; i < NUM_LOCS; ++i)
+        {
+            if(map[i].mine_exists == true)
+            {
+                map[i].mark_mine();
+                map[i].backgroundColor = UIColor.redColor();
+                map[i].timer.invalidate();
+            }
+        }
+    }
+    
+    func mark_location(var loc_id:Int)
+    {
+        if(GAME_STARTED == false)
+        {
+            if(loc_id == START_LOC) // dont start game until user presses start button
+            {
+                GAME_STARTED = true;
+                map[loc_id].setTitleColor(UIColor.clearColor(), forState: UIControlState.Normal);
+                mark_location(loc_id);
+            }
+        }
+        else if(!GAME_OVER)
+        {
+            if(map[loc_id].mine_exists)
+            {
+                // game is over
+                GAME_OVER = true;
+                end_game();
+                map[loc_id].setTitleColor(UIColor.redColor(), forState: UIControlState.Normal);
+                map[loc_id].backgroundColor = UIColor.blackColor();
+                println("You Lost! Game over...\n");
+            }
+            else if(!map[loc_id].explored)
+            {
+                map[loc_id].mark_explored();
+                ++COUNT;
+                if(won_game())
+                {
+                    println("You Won The game\n");
+                    GAME_OVER = true;
+                    end_game();
+                }
+            }
+            // generate mines
+            if(((NUM_LOCS - COUNT) > 1) && !GAME_OVER)
+            {
+                var neighbors = unmarked_neighbors(loc_id);
+                if(neighbors.count > 0)
+                {
+                    var temp_index:Int = Int(arc4random_uniform(UInt32(neighbors.count)));
+                    var index = neighbors[temp_index];
+                    if((index % 4) == 0)
+                    {
+                        map[index].speed = SPEED.FAST;
+                    }
+                    mark_mine(index);
+                }
+            }
+        }
     }
 }
 
